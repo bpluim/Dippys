4c256d0b0a7c9a1e504f614a783cd12d2d35c701
bpluim <ben.pluim@gmail.com>
2024-02-02 00:01:16 -0700

    navigation added

---
+++ src/app/about/page.js
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-white dark:bg-black">
+            <section className="w-full pt-12">    
+                <div className="mx-auto container space-y-12 px-4 md:px-6">
+                    <h1>About</h1>
+                    <p>This is the about page</p>
+                </div>
+            </section>
+        </main>
+    );
+};
---
+++ src/components/Header/index.js
@@ -0,0 +1,40 @@
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = ({...props}) => {
+  const { logo, navItems } = props;
+
+  return (
+    <header className="w-full h-2">
+      <div className="container mx-auto px-4 md:px-6 sticky">
+        <div className="flex items-center justify-between py-6">
+          <div>
+            <Link href="/">
+              <Image
+                alt="Dippys Logo"
+                className="aspect-video overflow-hidden object-cover"
+                height="50"
+                width="100"
+                src={logo}
+                priority
+              />
+            </Link>
+          </div>
+          <nav>
+            <div className="flex flex-row justify-end">
+              {navItems.map((item) => 
+                <div key={item.url} className="mx-2">
+                  <Link href={`/${item.url}`}>
+                    {item.label}
+                  </Link>
+                </div>
+              )}
+            </div>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
